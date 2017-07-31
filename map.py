e47019243031ba17012d47165b7e60e9fe6d848f
Fabien-B <fabien.bonneval@gmail.com>
2017-07-31 16:26:12 +0200

    Adds pathfinding algorithm.

--- map.py
+++ map.py
@@ -1,11 +1,28 @@
 from polygon import Polygon
 from point import Point
+from math import sqrt
+
+MAX_RECURSION_DEPTH = 5
 
 class Map(list):
     
     def get_obstacles(self, source: Point, dest: Point):
-        poly = get_closest(source, dest)        #type: None or Polygon
-        
+        found_paths = []
+        def g(p, path):
+            if len(path) > MAX_RECURSION_DEPTH:     #stop algo if recursion is too deep
+                return
+            poly = self.get_closest(p, dest)
+            if poly is None:                #if the objective is reachable in straight line, we have a path !
+                path += obj                 #if we want the objective in the path
+                found_path.append(path)     #we could also stop the algo here, but it will not be the shortest path
+            else:
+                t1, t2 = poly.get_tangente(p)   # get the "tangent" polygon points on the left and right side
+                g(t1, obj, path+t1)             # search path from these points to the objective while recording the path
+                g(t2, obj, path+t2)
+
+        g(dep, [dep])       # launch the search from the departure point. Initialise the path.
+
+        return min(found_paths, key=lambda x : compute_lenght(x))  # on retourne le chemin le plus court parmi ceux trouvés    
     
     def get_closest(self, source, dest: Point):
         closest = None
@@ -16,3 +33,12 @@ class Map(list):
                 min_dist = dist
                 closest = poly
         return closest
+    
+    @staticmethod
+    def compute_length(path):
+        lenght = 0
+        for i in range(len(path)-1):
+            dx = path[i+1].x + path[i].x
+            dy = path[i+1].y + path[i].y
+            lenght += sqrt(dx**2 + dy **2)
+        
